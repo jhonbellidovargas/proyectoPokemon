a2091164cd35d950c01b7b0a46313aac3615c490
fimovol <fimovolpe@gmail.com>
2022-12-27 22:28:53 -0500

    funcion de actualizar y borrar funcionando en perfil

--- js/profile.js
+++ js/profile.js
@@ -8,6 +8,7 @@ console.log(user_2);
 let userName = user_2.userName
 let nombre_usuario = user_2.name
 let email_de_usuario = user_2.email
+let id_del_usuario = user_2.id
 
 nombredeUsuario.value = userName
 nombre_completo.value = nombre_usuario
@@ -30,11 +31,62 @@ function borrarCuentas(e){
 document.getElementById("btnEliminarCuentasi").addEventListener("click",()=>{
     alert("se borro ")
     //borra aqui fetch
+    fetch(`https://proyectofinalm3backend-production.up.railway.app/api/users/${id_del_usuario}`, {
+        method: "DELETE",
+        headers: {
+        "Content-Type": "application/json",
+        },
+    })
+    .then((res) => res.json())
+    .then((res) => {
+    if (res.error) {
+        //ERROR
+    } else {
+        console.log(res);
+        localStorage.setItem('user', '');
+        window.location.href = "./login.html";
+    }
+    })
 })
 
 let modal = document.getElementById("btnActualizarCuenta")
 modal.addEventListener("click",()=>{
-    alert("se actualizo")
-    //actualiza aqui fetch
+    let nombredeUsuario = document.getElementById("nickname")
+    let nombre_completo = document.getElementById("name")
+    let email_completo = document.getElementById("email")
+    let contrasenia = document.getElementById("contrasenia")
+    let confirm_contrasenia = document.getElementById("confirm_contrasenia")
+    if(confirm_contrasenia.value != "" ){
+        if(confirm_contrasenia.value == contrasenia.value){
+            alert("se actualizo")
+            let objeto ={
+                name: nombre_completo.value,
+                lastName: nombre_completo.value,
+                email: email_completo.value,
+                userName: nombredeUsuario.value,
+                password: contrasenia.value
+            }
+            //actualiza aqui fetch
+            fetch(`https://proyectofinalm3backend-production.up.railway.app/api/users/${id_del_usuario}`, {
+                method: "PUT",
+                body: JSON.stringify(objeto),
+                headers: {
+                "Content-Type": "application/json",
+                },
+            })
+            .then((res) => res.json())
+            .then((res) => {
+            if (res.error) {
+                //ERROR
+            } else {
+                console.log(res);
+                window.location.href = "./pokemon_favoritos.html";
+            }
+            })
+        }
+    }
+    
+
+    
 })
 
