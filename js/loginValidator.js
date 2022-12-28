d453911e3eb52b3c67c2ee1ccb6841dd96b4ac88
Jhon Bellido <jhonbellidovargas@gmail.com>
2022-12-28 18:29:04 -0500

    2p font

--- js/loginValidator.js
+++ js/loginValidator.js
@@ -1,11 +1,11 @@
 // Obtenemos la ruta actual
 const currentPath = window.location.pathname;
-console.log("currentPath");
+console.log(currentPath);
 // Leemos el local storage para ver si hay un usuario logueado
 const user = JSON.parse(localStorage.getItem("user"));
 console.log("user");
 // Si no hay usuario logueado y la ruta actual no es el login o el registro, redirigimos al login
-if (!user && currentPath !== "/login.html" && currentPath !== "/registro.html" && currentPath !== "/index.html") {
+if (!user && currentPath !== "/login.html" && currentPath !== "/registro.html" && currentPath !== "/index.html" && currentPath !== "/") {
   window.location.href = "./login.html";
 } else if (user && (currentPath === "/login.html" || currentPath === "/registro.html" || currentPath === "/index.html")) {
   // Si hay usuario logueado y la ruta actual es el login, redirigimos al home
